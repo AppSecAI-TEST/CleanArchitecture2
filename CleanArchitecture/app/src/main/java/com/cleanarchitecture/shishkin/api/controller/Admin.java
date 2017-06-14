f4cb6b88249d3ae1e0606eeb174c6dc620dba536
Oleg Shishkin <shishkin1966@gmail.com>
2017-06-14 13:22:47 +0300

    оптимизация

--- CleanArchitecture/app/src/main/java/com/cleanarchitecture/shishkin/api/controller/Admin.java
+++ CleanArchitecture/app/src/main/java/com/cleanarchitecture/shishkin/api/controller/Admin.java
@@ -10,7 +10,6 @@ import com.cleanarchitecture.shishkin.api.repository.Repository;
 import com.cleanarchitecture.shishkin.api.storage.DiskCache;
 import com.cleanarchitecture.shishkin.api.storage.MemoryCache;
 import com.cleanarchitecture.shishkin.api.usecases.UseCasesController;
-import com.cleanarchitecture.shishkin.application.app.ApplicationController;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
@@ -70,7 +69,7 @@ public class Admin extends AbstractAdmin {
     public synchronized void register() {
         isFinishApplication = false;
 
-        final Context context = ApplicationController.getInstance();
+        final Context context = AdminUtils.getContext();
 
         // default Singleton controllers
         registerModule(ErrorController.getInstance());
