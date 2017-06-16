99ccc94677d3489bd2e93771f54d52bdaff3e383
Oleg Shishkin <shishkin1966@gmail.com>
2017-06-16 03:10:00 +0300

    оптимизация

--- mokey.cmd
+++ mokey.cmd
@@ -1,1 +1,1 @@
-H:\sdk\platform-tools\adb shell monkey -p com.cleanarchitecture.shishkin -v 16000 --ignore-timeouts --throttle 1000 
+H:\sdk\platform-tools\adb shell monkey -p com.cleanarchitecture.shishkin -v 24000 --ignore-timeouts --throttle 1000 
