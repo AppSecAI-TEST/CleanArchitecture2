8f92ddf14c710442d224be7aaa0787c5c5d3e5b4
Oleg Shishkin <shishkin1966@gmail.com>
2017-06-20 11:57:04 +0300

    доработка

--- CleanArchitecture/app/src/main/java/com/cleanarchitecture/shishkin/application/ui/fragment/HomeFragment.java
+++ CleanArchitecture/app/src/main/java/com/cleanarchitecture/shishkin/application/ui/fragment/HomeFragment.java
@@ -161,7 +161,7 @@ public class HomeFragment extends AbstractContentFragment implements ILocationSu
     public synchronized void onToolbarClickEvent(OnToolbarClickEvent event) {
         if (event.getView() != null && event.getView().getId() == R.id.item) {
             final ShareUtil.ShareData shareData = new ShareUtil.ShareData(null, getString(R.string.test_mesage));
-            ShareUtil.performShareWithImage(shareData, AdminUtils.getActivity(), null);
+            ShareUtil.performShare(shareData, AdminUtils.getActivity(), null);
         }
     }
 
