9c4cacad27378309d12a83be3099a0f95be7f006
Oleg Shishkin <shishkin1966@gmail.com>
2017-06-23 08:19:12 +0300

    оптимизация

--- monkey.cmd
+++ monkey.cmd
@@ -1,1 +1,1 @@
-H:\sdk\platform-tools\adb shell monkey -p com.cleanarchitecture.shishkin -v 40000 --ignore-timeouts --throttle 1000 --monitor-native-crashes >1.txt
+H:\sdk\platform-tools\adb shell monkey -p com.cleanarchitecture.shishkin -v 40000 --ignore-timeouts --throttle 1000 --monitor-native-crashes
