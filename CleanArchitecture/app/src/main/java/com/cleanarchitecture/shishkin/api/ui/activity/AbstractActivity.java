b0c4518480c1963796baa243337b07ce035acd9d
Oleg Shishkin <shishkin1966@gmail.com>
2017-06-26 08:27:38 +0300

    оптимизация

--- CleanArchitecture/app/src/main/java/com/cleanarchitecture/shishkin/api/ui/activity/AbstractActivity.java
+++ CleanArchitecture/app/src/main/java/com/cleanarchitecture/shishkin/api/ui/activity/AbstractActivity.java
@@ -67,7 +67,6 @@ public abstract class AbstractActivity extends LifecycleActivity
         AdminUtils.register(this);
 
         mStateMachine.setState(Lifecycle.STATE_CREATE);
-
     }
 
     @Override
