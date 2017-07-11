1875f4c712caa02e7d6447d54c298209f3d63648
Oleg Shishkin <shishkin1966@gmail.com>
2017-07-11 16:55:04 +0300

    Обновлена документация

--- CleanArchitecture/app/src/main/java/com/cleanarchitecture/shishkin/api/storage/IImageDiskCache.java
+++ CleanArchitecture/app/src/main/java/com/cleanarchitecture/shishkin/api/storage/IImageDiskCache.java
@@ -2,20 +2,56 @@ package com.cleanarchitecture.shishkin.api.storage;
 
 import android.graphics.Bitmap;
 
+/**
+ * The interface Image disk cache.
+ */
 public interface IImageDiskCache {
 
+    /**
+     * Put bitmap to cache
+     *
+     * @param key    the key
+     * @param bitmap the bitmap
+     */
     void put(String key, Bitmap bitmap);
 
+    /**
+     * Put bitmap to cache with expired period
+     *
+     * @param key     the key
+     * @param bitmap  the bitmap
+     * @param expired the expired
+     */
     void put(String key, Bitmap bitmap, long expired);
 
+    /**
+     * Get bitmap from cache
+     *
+     * @param key the key
+     * @return the bitmap
+     */
     Bitmap get(String key);
 
+    /**
+     * Remove bitmap from cache
+     *
+     * @param key the key
+     */
     void clear(String key);
 
+    /**
+     * Clear all bitmaps
+     */
     void clear();
 
+    /**
+     * Flush cache
+     */
     void flush();
 
+    /**
+     * Close cache
+     */
     void close();
 
 }
