fac9af719f9193416b5f72d44927dcda5c2dc6b4
Oleg Shishkin <shishkin1966@gmail.com>
2017-07-18 11:37:07 +0300

    Доработаны валидаторы

--- CleanArchitecture/app/src/main/java/com/cleanarchitecture/shishkin/application/validate/PhoneContactItemValidator.java
+++ CleanArchitecture/app/src/main/java/com/cleanarchitecture/shishkin/application/validate/PhoneContactItemValidator.java
@@ -1,5 +1,11 @@
 package com.cleanarchitecture.shishkin.application.validate;
 
+import android.content.Context;
+
+import com.cleanarchitecture.shishkin.R;
+import com.cleanarchitecture.shishkin.api.controller.AdminUtils;
+import com.cleanarchitecture.shishkin.api.repository.data.ExtError;
+import com.cleanarchitecture.shishkin.api.repository.data.Result;
 import com.cleanarchitecture.shishkin.api.validate.AbstractValidator;
 import com.cleanarchitecture.shishkin.application.data.item.PhoneContactItem;
 import com.cleanarchitecture.shishkin.common.utils.StringUtils;
@@ -8,34 +14,43 @@ public class PhoneContactItemValidator extends AbstractValidator {
 
     public static final String NAME = PhoneContactItemValidator.class.getName();
 
-    public PhoneContactItemValidator() {
-    }
-
     @Override
-    public boolean validate(final Object object) {
+    public Result<Boolean> validate(final Object object) {
+        final Result<Boolean> result = new Result<>();
+        result.setResult(false);
+
         if (object == null) {
-            return false;
+            return result;
         }
 
         if (object instanceof PhoneContactItem) {
             final PhoneContactItem item = (PhoneContactItem) object;
             final String phones = item.getPhones();
-            if (StringUtils.isNullOrEmpty(phones)) {
-                return false;
+            if (!StringUtils.isNullOrEmpty(phones)) {
+                result.setResult(true);
             }
         } else if (object instanceof String) {
             final String phone = (String) object;
-            if (StringUtils.isNullOrEmpty(phone)) {
-                return false;
-            } else if (phone.length() < 5) {
-                return false;
+            if (!StringUtils.isNullOrEmpty(phone)) {
+                final int length = StringUtils.getDigits(phone).length();
+                if (length == 7 || length == 11) {
+                    result.setResult(true);
+                } else if (length == 3) {
+                    result.setResult(true);
+                } else if (length > 11) {
+                    result.setResult(false);
+                    final Context context = AdminUtils.getContext();
+                    if (context != null) {
+                        result.setError(new ExtError().setErrorText(NAME, context.getString(R.string.error_phone_max_length, phone)));
+                    }
+                }
             }
         }
-        return true;
+        return result;
     }
 
     @Override
     public String getName() {
-        return null;
+        return NAME;
     }
 }
