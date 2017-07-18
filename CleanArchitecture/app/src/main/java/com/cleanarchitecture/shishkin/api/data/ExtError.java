9e2017c3882a3dfa6cc4fbe8343c7dca27e90964
Oleg Shishkin <shishkin1966@gmail.com>
2017-07-18 15:12:00 +0300

    Оптимизация кода

--- CleanArchitecture/app/src/main/java/com/cleanarchitecture/shishkin/api/data/ExtError.java
+++ CleanArchitecture/app/src/main/java/com/cleanarchitecture/shishkin/api/data/ExtError.java
@@ -4,16 +4,14 @@ import com.cleanarchitecture.shishkin.api.controller.AdminUtils;
 import com.cleanarchitecture.shishkin.api.controller.ErrorController;
 import com.cleanarchitecture.shishkin.common.utils.StringUtils;
 
-public class ExtError implements IExtError {
+public class ExtError {
     private StringBuilder mErrorText = new StringBuilder();
     private String mSender = null;
 
-    @Override
     public String getErrorText() {
         return mErrorText.toString();
     }
 
-    @Override
     public ExtError setError(final String sender, final String error) {
         mSender = sender;
         addError(error);
@@ -29,7 +27,6 @@ public class ExtError implements IExtError {
         }
     }
 
-    @Override
     public ExtError setError(final String sender, final Exception e, final String error) {
         if (e != null) {
             ErrorController.getInstance().onError(sender, e);
@@ -40,7 +37,6 @@ public class ExtError implements IExtError {
         return this;
     }
 
-    @Override
     public ExtError setError(final String sender, final Exception e, final int code) {
         if (e != null) {
             ErrorController.getInstance().onError(sender, e);
@@ -51,7 +47,6 @@ public class ExtError implements IExtError {
         return this;
     }
 
-    @Override
     public ExtError setError(final String sender, final int code) {
         if (code != 0) {
             mSender = sender;
@@ -60,17 +55,14 @@ public class ExtError implements IExtError {
         return this;
     }
 
-    @Override
     public boolean hasError() {
         return mErrorText.length() > 0;
     }
 
-    @Override
     public String getSender() {
         return mSender;
     }
 
-    @Override
     public ExtError setSender(final String sender) {
         mSender = sender;
         return this;
